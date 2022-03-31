8fe218bc00fbdabfd36d394da8dd64ae35a33e90
Johannes C. Mayer <j.c.mayer240@gmail.com>
2022-03-31 16:36:39 +0200

    code cleanup; editing flag

--- bookletify.py
+++ bookletify.py
@@ -5,8 +5,16 @@ from subprocess import check_output
 from shutil import which, copy
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
-TITLE_PAGE_PATH_NO_EX = f"{SCRIPT_PATH}/templates/title"
-BLANK_PAGE_PATH_NO_EX = f"{SCRIPT_PATH}/templates/blank"
+TITLE_PAGE_TEX_PATH = f"{SCRIPT_PATH}/templates/title.tex"
+BLANK_PAGE_TEX_PATH = f"{SCRIPT_PATH}/templates/blank.tex"
+
+def num_of_pages(pdf_path):
+    num_pages = check_output(f"pdftk {pdf_path} dump_data | grep NumberOfPages | awk '{{print $2}}'", shell=True)
+    return int(num_pages)
+
+def build_latex(tex_path):
+    os.system(f"pdflatex {tex_path}")
+    return os.path.splitext(tex_path)[0] + ".pdf"
 
 def is_pdf(string):
     if os.path.isfile(string) and os.path.splitext(string)[-1] == ".pdf":
@@ -31,39 +39,18 @@ def wrap_book(content_pdf_path, title_pdf_path, blank_pdf_path):
         os.remove(p)
     return file_path
 
-def num_of_pages(pdf_file_path):
-    num_pages = check_output(f"pdftk {pdf_file_path} dump_data | grep NumberOfPages | awk '{{print $2}}'", shell=True)
-    return int(num_pages)
+def bookletify(pdf_path, output_path):
+    num_pages = num_of_pages(pdf_path)
 
-def main():
-    for p in ["pdftk", "pdfnup"]:
-        assert which(p) is not None, f"Script needs '{p}' executables in path."
+    assert num_pages % 4 == 0, "Only can handle pdf with page count divisible by 4"
+    assert num_pages % 2 == 0
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest="input_path", required=True, type=is_pdf, help="Input pdf file.")
-    parser.add_argument('-o', '--output', dest="output_path", required=True, help="output pdf file.")
-    args = parser.parse_args()
-
-    print("Building templates")
-    os.system(f"pdflatex {BLANK_PAGE_PATH_NO_EX}.tex")
-    blank_page_pdf = f"{BLANK_PAGE_PATH_NO_EX}.pdf"
-    os.system(f"pdflatex {TITLE_PAGE_PATH_NO_EX}.tex")
-    title_page_pdf = f"{TITLE_PAGE_PATH_NO_EX}.pdf"
-
-    print("Wraping book")
-    wraped_pdf = wrap_book(args.input_path, title_page_pdf, blank_page_pdf)
-
-    print("Bookletify wraped book")
-    num_pages = num_of_pages(wraped_pdf)
-
-    print(f"Number of pages in pdf: {num_pages}")
+    print(f"Number of logical pages: {num_pages}")
+    print(f"Number of printed pages: {num_pages/4}")
 
     l = []
 
     swap = True
-
-    assert num_pages % 4 == 0, "Only can handle pdf with page count divisible by 4"
-    assert num_pages % 2 == 0
 
     for i in range(1, int(num_pages/2) + 1):
         if swap:
@@ -81,11 +68,40 @@ def main():
         if i < len(l) - 1:
             num_str += " "
 
-    temp_file = f"temp_{args.output_path}"
+    temp_file = f"temp_{output_path}"
 
-    os.system(f"pdftk {wraped_pdf} cat {num_str} output {temp_file}")
-    os.system(f"pdfnup --nup 2x1 --outfile {args.output_path} {temp_file}")
+    os.system(f"pdftk {pdf_path} cat {num_str} output {temp_file}")
+    os.system(f"pdfnup --nup 2x1 --outfile {output_path} {temp_file}")
     os.remove(temp_file)
+
+def edit(path):
+    editor = check_output("echo $EDITOR", shell=True)
+    editor = editor.decode("utf-8").strip()
+    os.system(f"{editor} {path}")
+
+def main():
+    for p in ["pdftk", "pdfnup", "pdflatex"]:
+        assert which(p) is not None, f"Script needs '{p}' executables in path."
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', dest="input_path", required=True, type=is_pdf, help="Input pdf file.")
+    parser.add_argument('-o', '--output', dest="output_path", required=True, help="output pdf file.")
+    parser.add_argument('-ed', '--edit', dest="edit", action="store_true", default=False, help="Edit title.tex")
+    args = parser.parse_args()
+
+    if args.edit:
+        edit(TITLE_PAGE_TEX_PATH)
+        exit(0)
+
+    print("Building templates")
+    blank_page_pdf_path = build_latex(BLANK_PAGE_TEX_PATH)
+    title_page_pdf_path = build_latex(TITLE_PAGE_TEX_PATH)
+
+    print("Wraping book")
+    wraped_pdf_path = wrap_book(args.input_path, title_page_pdf_path, blank_page_pdf_path)
+
+    print("Bookletify wraped book")
+    bookletify(wraped_pdf_path, args.output_path)
 
 if __name__ == "__main__":
     main()
